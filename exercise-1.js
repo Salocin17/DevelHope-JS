e45d08ffab3b96718e3a4427f44e754d3244ba46
Salocin17 <nikolaszarone@gmail.com>
2023-02-10 17:45:35 +0100

    JS-objects

---
+++ exercise-1.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person))
+
+//Ammetto di non aver capito cosa volesse di preciso l'esercizio 
