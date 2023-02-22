445b68e7f0cca44bbbb117111c73c34e247a44fd
Salocin17 <nikolaszarone@gmail.com>
2023-02-22 20:07:25 +0100

    fix

--- exercise-1.js
+++ exercise-1.js
@@ -1,5 +1,7 @@
-function sum(...numbers) {
-    return numbers.reduce((a, b) => a + b);
-}
+//function sum(...numbers) {
+//   return numbers.reduce((a, b) => a + b);
+//}
+
+const sum = (...numbers) => numbers.reduce((a, b) => a + b);
 
 console.log(sum(1, 2, 3, 4, 5));
