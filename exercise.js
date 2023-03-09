27ca7d6dd0a5616bca1a88ddca9ff8f20d8a767f
Salocin17 <nikolaszarone@gmail.com>
2023-03-09 17:45:07 +0100

    JS-promise-cahining

--- exercise.js
+++ exercise.js
@@ -1,11 +1,27 @@
-const number = 15;
+const isLogged = true;
 
-const myPromise = new Promise ((resolve, reject) => {
-    number > 10 ? resolve(number) : reject(error);
-    } );
 
-myPromise.then(() => console.log('success'))
-        .catch(() => console.error('error'));
+const randomNum = new Promise((resolve, reject) => {
+        isLogged ? resolve(Math.random()) : reject("error");
+});
+
+
+randomNum.then((value) => {
+    const objectNum = new Promise((resolve, reject) => {
+        value > 0.5 ? resolve({name: "John", age: 24}) : reject('errore');
+    })
+    objectNum.then((value) => {
+        console.log(value);
+    })
+    .catch((err) => console.error(err));
+})
+.catch((err) => console.error(err));
+
+
+
+
+
+
 
 
 
