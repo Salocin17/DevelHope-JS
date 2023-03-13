18bb49f2bd7c3b2f5792d036ff68a0b4d8a81732
Salocin17 <nikolaszarone@gmail.com>
2023-03-13 11:46:24 +0100

    JS-promise-err

--- exercise.js
+++ exercise.js
@@ -1,18 +1,17 @@
 const isLogged = true;
 
 const randomNum = new Promise((resolve, reject) => {
-    isLogged ? resolve(Math.random()) : reject("error");
+    isLogged ? resolve(Math.random()) : reject(new Error("error"));
 });
 
 randomNum
     .then((value) => {
         const objectNum = new Promise((resolve, reject) => {
-            value > 0.5 ? resolve({ name: "John", age: 24 }) : reject("errore");
+            value > 0.5 ? resolve({ name: "John", age: 24 }) : reject(new Error("error"));
         });
         objectNum
-            .then((value) => {
-                console.log(value);
-            })
+            .then((value) => { console.log(value) })
             .catch((err) => console.error(err));
     })
     .catch((err) => console.error(err));
+    .finally(() => console.log("Complete"));
