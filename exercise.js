fa12e209a2e8980db84167030d31bc7dfb7969d3
Salocin17 <nikolaszarone@gmail.com>
2023-03-13 12:04:32 +0100

    fix

--- exercise.js
+++ exercise.js
@@ -1,17 +1,22 @@
 const isLogged = true;
 
 const randomNum = new Promise((resolve, reject) => {
-    isLogged ? resolve(Math.random()) : reject(new Error("error"));
+  isLogged ? resolve(Math.random()) : reject(new Error("error"));
 });
 
+randomNum;
 randomNum
-    .then((value) => {
-        const objectNum = new Promise((resolve, reject) => {
-            value > 0.5 ? resolve({ name: "John", age: 24 }) : reject(new Error("error"));
-        });
-        objectNum
-            .then((value) => { console.log(value) })
-            .catch((err) => console.error(err));
-    })
-    .catch((err) => console.error(err));
-    .finally(() => console.log("Complete"));
+  .then((value) => {
+    const objectNum = new Promise((resolve, reject) => {
+      value > 0.5
+        ? resolve({ name: "John", age: 24 })
+        : reject(new Error("error"));
+    });
+    objectNum
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((err) => console.error(err));
+  })
+  .catch((err) => console.error(err))
+  .finally(() => console.log("finally"));
